fc9e22bec6505051fdbadecda37a414a49d9cdd9
adammcgrogan <adam.mcgrogan@dailypay.com>
2026-04-09 08:04:26 +0100

    seed templates. industry specific templates state.

--- internal/handlers/home.go
+++ internal/handlers/home.go
@@ -11,23 +11,33 @@ func (h *Handler) Home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	type templateEntry struct {
-		ID          string
-		Name        string
-		Description string
-		ExampleSlug string
-		ExampleURL  string
-	}
+	var general []templateEntry
+	industryMap := make(map[string][]templateEntry)
+	var industryOrder []string
 
-	entries := make([]templateEntry, len(siteTemplates))
-	for i, t := range siteTemplates {
-		entries[i] = templateEntry{
+	for _, t := range siteTemplates {
+		entry := templateEntry{
 			ID:          t.ID,
 			Name:        t.Name,
 			Description: t.Description,
-			ExampleSlug: t.ExampleSlug,
 			ExampleURL:  h.exampleURL(t.ExampleSlug),
 		}
+		if t.Industry == "" {
+			general = append(general, entry)
+		} else {
+			if _, seen := industryMap[t.Industry]; !seen {
+				industryOrder = append(industryOrder, t.Industry)
+			}
+			industryMap[t.Industry] = append(industryMap[t.Industry], entry)
+		}
+	}
+
+	var industryGroups []industryGroup
+	for _, name := range industryOrder {
+		industryGroups = append(industryGroups, industryGroup{
+			Name:      name,
+			Templates: industryMap[name],
+		})
 	}
 
 	tmpl := template.Must(template.ParseFiles(
@@ -35,6 +45,7 @@ func (h *Handler) Home(w http.ResponseWriter, r *http.Request) {
 		"web/templates/public/home.html",
 	))
 	tmpl.ExecuteTemplate(w, "base", map[string]any{
-		"Templates": entries,
+		"GeneralTemplates": general,
+		"IndustryGroups":   industryGroups,
 	})
 }
